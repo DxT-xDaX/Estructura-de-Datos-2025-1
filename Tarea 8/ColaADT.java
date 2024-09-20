d3c28fb68a83845e04ff8927c42a121007c31eb5
DxT-xDaX <axelyahirmrdez@gmail.com>
2024-09-19 23:48:31 -0600

    Create ColaADT.java

---
+++ Tarea 8/ColaADT.java
@@ -0,0 +1,40 @@
+package unam.mx.colas;
+
+import java.util.LinkedList;
+
+public class ColaADT<E> {
+    private LinkedList<E> data;
+
+    public ColaADT() {
+        this.data = new LinkedList<>();
+    }
+
+    public boolean estaVacia(){
+        boolean res = false;
+        if (this.data.size() == 0){
+            res = true;
+        }
+        return res;
+    }
+
+    public int longitud(){
+        return this.data.size();
+    }
+
+    public E frente(){
+        return this.data.getFirst();
+    }
+
+    public void encolar(E valor){ //enqueue
+        this.data.addLast(valor);
+    }
+
+    public E desEncolar(){
+        return this.data.removeFirst();
+    }
+
+    @Override
+    public String toString() {
+        return data.toString();  // Deja que LinkedList implemente su propia representación
+    }
+}
